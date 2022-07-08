e6f0fa90d64100ff3d77b5005562283d7fafd391
keisukefukuchi <kekke53530708@gmail.com>
2022-07-08 17:02:05 +0900

    Fix: CommonHeader View

--- components/CommonHeader.vue
+++ components/CommonHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <header>
-    <h1 class="title">{{ $route.name }}</h1>
-    <nav class="nav">
-      <ul class="menu-group">
-        <li class="menu-item" v-if="!$auth.loggedIn">
+  <header class="header">
+    <h1 class="header__title">Atte</h1>
+    <nav class="header__nav">
+      <ul class="nav__menu">
+        <li class="nav__menu-list" v-if="!$auth.loggedIn">
           <NuxtLink to="/register">Register</NuxtLink>
         </li>
-        <li class="menu-item" v-if="!$auth.loggedIn">
+        <li class="nav__menu-list" v-if="!$auth.loggedIn">
           <NuxtLink to="/login">Login</NuxtLink>
         </li>
-        <li class="menu-item" v-else>
+        <li class="nav__menu-list" v-else>
           <a @click="logout">Logout</a>
         </li>
       </ul>
@@ -32,24 +32,26 @@ export default {
 };
 </script>
 
-<style scoped>
-header {
+<style>
+.header {
   display: flex;
+  justify-content: space-between;
   height: 100px;
   padding: 0 50px;
   margin-bottom: 30px;
-  background-color: darkgrey;
+  background-color: #ffffff;
   align-items: center;
 }
-.title {
+.header__title {
   margin-right: auto;
+  font-size: 32px;
 }
-.menu-item {
+.nav__menu-list {
   list-style: none;
   display: inline-block;
   padding: 10px;
 }
-.menu-item a {
+.nav__menu-list a {
   color: black;
   text-decoration: none;
   cursor: pointer;
