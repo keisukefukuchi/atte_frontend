859258f17b4ef2163ab37902bbd04e166758e121
keisukefukuchi <kekke53530708@gmail.com>
2022-07-10 12:33:02 +0900

    Add: login-function

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,41 @@
 <template>
-  <form @submit.prevent="login">
-    <input type="email" v-model="email" placeholder="email" required />
-    <input type="password" v-model="password" placeholder="password" required />
-    <button type="submit">送信</button>
-  </form>
+   <validation-observer ref="obs" v-slot="ObserverProps">
+    <form @submit.prevent="login" class="inner-elements">
+      <validation-provider v-slot="{ errors }" rules="required|email">
+         <div class="error">{{ errors[0] }}</div>
+         <div class="element">
+           <input
+             type="email"
+             v-model="email"
+             placeholder="email"
+             id="email"
+           />
+         </div>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        vid="passwordConfirm"
+        rules="required"
+      >
+      <div class="error">{{ errors[0] }}</div>
+      <div class="element">
+        <input
+          type="password"
+          v-model="password"
+          placeholder="password"
+          id="password"
+        />
+      </div>
+      </validation-provider>
+      <button
+        type="submit"
+        class="login-button"
+        :disabled="ObserverProps.invalid || !ObserverProps.validated"
+      >
+        ログイン
+      </button>
+    </form>
+  </validation-observer>
 </template>
 
 <script>
@@ -17,15 +49,18 @@ export default {
   methods: {
     async login() {
       try {
-        await this.$auth.loginWith("laravelJWT", {
+        const res = await this.$auth.loginWith("laravelJWT", {
           data: {
             email: this.email,
             password: this.password,
-          },
+          }
         });
         this.$router.push("/");
-      } catch {
+      } catch(error)  {
+        console.log(error);
         alert("メールアドレスまたはパスワードが間違っております");
+        this.loginEmail = "";
+        this.loginPassword = "";
       }
     },
   },
