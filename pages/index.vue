b56558f3518d329e461c5f260045826cb33b8e1a
keisukefukuchi <kekke53530708@gmail.com>
2022-07-11 12:32:00 +0900

    Add: restEnd function

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <button @click="attendanceStart">勤務開始</button>
     <button @click="attendanceEnd">勤務終了</button>
     <button @click="restStart">休憩開始</button>
-    <button>休憩終了</button>
+    <button @click="restEnd">休憩終了</button>
   </div>
 </template>
 
@@ -47,6 +47,23 @@ export default {
          'user_id': this.$auth.user.id,
       };
       const resData = await this.$axios.post("http://localhost:8000/api/auth/rest",sendData);
+      console.log(resData);
+      this.attendanceGet();
+    },
+    async restEnd() {
+      const user_id = this.$auth.user.id;
+      const today = this.$dayjs().format('YYYY-MM-DD');
+      const attendance = this.attendanceLists.find(function (element) {
+        if(element.user_id == user_id && element.date == today) {
+          return element;
+        }
+      });
+      const rest = attendance.rests.find(function (element) {
+        if(element.end_time == null) {
+          return element;
+        }
+      });
+      const resData = await this.$axios.put("http://localhost:8000/api/auth/rest/"+ rest.id);
       console.log(resData);
       this.attendanceGet();
     },
